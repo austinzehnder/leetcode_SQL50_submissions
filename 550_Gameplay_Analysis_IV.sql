09fd58c6d15e8fc690f2df968dc5852db07c36dd
Austin L. Zehnder <134451398+austinzehnder@users.noreply.github.com>
2024-02-02 14:46:17 -0800

    Create 550_Gameplay_Analysis_IV.sql

---
+++ 550_Gameplay_Analysis_IV.sql
@@ -0,0 +1,13 @@
+-- Postgresql code
+WITH first_login AS (
+  SELECT player_id, MIN(event_date) AS first_login_date
+  FROM activity
+  GROUP BY player_id
+)
+
+SELECT 
+  ROUND(COUNT(DISTINCT activity.player_id)::numeric/COUNT(DISTINCT first_login.player_id), 2) AS fraction
+FROM first_login
+LEFT JOIN activity ON 
+  first_login.player_id = Activity.player_id
+  AND event_date = first_login_date + INTERVAL '1 day'
