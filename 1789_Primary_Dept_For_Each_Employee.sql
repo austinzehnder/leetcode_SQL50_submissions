3e2e981a0023a318c87550e684e30cfb5a83b391
Austin L. Zehnder <134451398+austinzehnder@users.noreply.github.com>
2024-02-28 11:01:59 -0800

    Update 1789_Primary_Dept_For_Each_Employee.sql

--- 1789_Primary_Dept_For_Each_Employee.sql
+++ 1789_Primary_Dept_For_Each_Employee.sql
@@ -1,28 +1,14 @@
-WITH PrimaryDept AS (
-    SELECT
-        employee_id,
-        MIN(department_id) AS primary_department
-    FROM
-        Employee
-    WHERE
-        primary_flag = 'Y'
-    GROUP BY
-        employee_id
-)
-SELECT
-    e.employee_id,
-    COALESCE(pd.primary_department, e.department_id) AS department_id
-FROM
-    Employee e
-LEFT JOIN
-    PrimaryDept pd ON e.employee_id = pd.employee_id
-WHERE
-    e.primary_flag = 'Y'
-   OR (
-        e.primary_flag = 'N'
-        AND NOT EXISTS (
-            SELECT 1
-            FROM Employee
-            WHERE employee_id = e.employee_id AND primary_flag = 'Y'
-        )
-    );
+SELECT 
+  employee_id, 
+  department_id 
+FROM 
+  (
+    SELECT 
+      *, 
+      COUNT(employee_id) OVER(PARTITION BY employee_id) AS EmployeeCount
+    FROM 
+      Employee
+  ) EmployeePartition 
+WHERE 
+  EmployeeCount = 1 
+  OR primary_flag = 'Y';
