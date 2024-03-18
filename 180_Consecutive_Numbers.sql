dd4a9c5b0553eedf86e93a207dcba098ce9ea704
Austin L. Zehnder <134451398+austinzehnder@users.noreply.github.com>
2024-03-18 13:06:07 -0700

    Create 180_Consecutive_Numbers.sql

---
+++ 180_Consecutive_Numbers.sql
@@ -0,0 +1,11 @@
+SELECT DISTINCT num AS ConsecutiveNums
+FROM (
+    SELECT 
+        num,
+        LEAD(num, 1) OVER (ORDER BY id) AS next_num,
+        LEAD(num, 2) OVER (ORDER BY id) AS next_next_num
+    FROM 
+        Logs
+) AS t
+WHERE
+    num = next_num AND num = next_next_num;
